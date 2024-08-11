f8db7d0c2363d4cff2eb8680f466a35f0c92779d
habib <bigharassane.elhabib@gmail.com>
2024-08-11 23:14:47 +0100

    Your commit message

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WelcomeMessage from './WelcomeMessage'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -28,7 +29,12 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+            <WelcomeMessage />
+            {/* Other components or content */}
+        </div>
     </>
+    
   )
 }
 
