c9e61da858ce06427b853dab8ba0a5c225ce015e
habib <bigharassane.elhabib@gmail.com>
2024-08-18 23:52:39 +0100

    enter

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,19 +2,57 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ 
-      display: 'flex', 
-      justifyContent: 'space-between', 
-      alignItems: 'center', 
-      backgroundColor: 'navy', 
-      padding: '10px'
-    }}>
+    <nav 
+      style={{ 
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        alignItems: 'center', 
+        backgroundColor: 'navy', 
+        padding: '10px'
+      }}
+    >
       <div style={{ color: 'white' }}>My Company</div>
       <div>
-        <Link to="/" style={{ margin: '0 10px', color: 'white', textDecoration: 'none' }}>Home</Link>
-        <Link to="/about" style={{ margin: '0 10px', color: 'white', textDecoration: 'none' }}>About</Link>
-        <Link to="/services" style={{ margin: '0 10px', color: 'white', textDecoration: 'none' }}>Services</Link>
-        <Link to="/contact" style={{ margin: '0 10px', color: 'white', textDecoration: 'none' }}>Contact</Link>
+        <Link 
+          to="/" 
+          style={{ 
+            margin: '0 10px', 
+            color: 'white', 
+            textDecoration: 'none' 
+          }}
+        >
+          Home
+        </Link>
+        <Link 
+          to="/about" 
+          style={{ 
+            margin: '0 10px', 
+            color: 'white', 
+            textDecoration: 'none' 
+          }}
+        >
+          About
+        </Link>
+        <Link 
+          to="/services" 
+          style={{ 
+            margin: '0 10px', 
+            color: 'white', 
+            textDecoration: 'none' 
+          }}
+        >
+          Services
+        </Link>
+        <Link 
+          to="/contact" 
+          style={{ 
+            margin: '0 10px', 
+            color: 'white', 
+            textDecoration: 'none' 
+          }}
+        >
+          Contact
+        </Link>
       </div>
     </nav>
   );
