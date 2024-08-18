ffb42865dfb6b29f412ae3dd0f83db25677df9f1
habib <bigharassane.elhabib@gmail.com>
2024-08-18 18:08:14 +0100

    second push

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
@@ -9,7 +10,11 @@ import './App.css'
 
 function App() {
   return (
-      <>
+      <> <UserProfile 
+      name="Alice" 
+      age="25" 
+      bio="Loves hiking and photography" 
+  />
          <Header />
          <MainContent />
          <Footer />
