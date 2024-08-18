87735ce906991465d9cd3ef140865cd04666ca5d
habib <bigharassane.elhabib@gmail.com>
2024-08-18 19:16:37 +0100

    correction

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,9 +1,12 @@
-import ProfilePage from './components/ProfilePage';
-
 function App() {
-  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
-
-  return <ProfilePage userData={userData} />;
-}
-
-export default App;
+    const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  
+    return (
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+    );
+  }
+  
+  export default App;
+  
