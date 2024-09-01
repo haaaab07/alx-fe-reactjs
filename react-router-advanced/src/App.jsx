ee8400177064717dde0656e64500b21f4749540f
habib <bigharassane.elhabib@gmail.com>
2024-09-01 22:19:53 +0100

    ggg

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,16 +1,32 @@
-import ProtectedRoute from './components/ProtectedRoute';
+// src/App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import ProfileDetails from './pages/ProfileDetails';
+import ProfileSettings from './pages/ProfileSettings';
+import BlogPost from './pages/BlogPost';
+import NotFound from './pages/NotFound';
 import Login from './pages/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Home />} />
-                <Route path="/profile/*" element={<ProtectedRoute element={<Profile />} />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/profile/*" element={
+                    <ProtectedRoute element={<Profile />} />
+                }>
+                    <Route path="details" element={<ProfileDetails />} />
+                    <Route path="settings" element={<ProfileSettings />} />
+                </Route>
                 <Route path="/blog/:id" element={<BlogPost />} />
+                <Route path="/login" element={<Login />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </Router>
     );
 }
+
+export default App;
