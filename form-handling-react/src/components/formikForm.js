4a9ba76f28d5688e62ed25dca01ef451b78496aa
habib <bigharassane.elhabib@gmail.com>
2024-09-01 20:23:12 +0100

    fff

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -1,65 +1,71 @@
 import React from 'react';
-import { useFormik, ErrorMessage } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
+const validationSchema = Yup.object({
+    username: Yup.string()
+        .required('Username is required'),
+    email: Yup.string()
+        .email('Invalid email address')
+        .required('Email is required'),
+    password: Yup.string()
+        .required('Password is required')
+});
+
 function FormikForm() {
-    const formik = useFormik({
-        initialValues: {
-            username: '',
-            email: '',
-            password: ''
-        },
-        validationSchema: Yup.object({
-            username: Yup.string()
-                .required('Username is required'),
-            email: Yup.string()
-                .email('Invalid email address')
-                .required('Email is required'),
-            password: Yup.string()
-                .required('Password is required')
-        }),
-        onSubmit: values => {
-            console.log('Form submitted', values);
-        },
-    });
+    const initialValues = {
+        username: '',
+        email: '',
+        password: ''
+    };
+
+    const handleSubmit = (values) => {
+        console.log('Form submitted', values);
+    };
 
     return (
-        <form onSubmit={formik.handleSubmit}>
-            <div>
-                <label htmlFor="username">Username:</label>
-                <input
-                    id="username"
-                    type="text"
-                    {...formik.getFieldProps('username')}
-                />
-                <ErrorMessage name="username">
-                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
-                </ErrorMessage>
-            </div>
-            <div>
-                <label htmlFor="email">Email:</label>
-                <input
-                    id="email"
-                    type="email"
-                    {...formik.getFieldProps('email')}
-                />
-                <ErrorMessage name="email">
-                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
-                </ErrorMessage>
-            </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input
-                    id="password"
-                    type="password"
-                    {...formik.getFieldProps('password')}
-                />
-                <ErrorMessage name="password">
-                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
-                </ErrorMessage>
-            </div>
-            <button type="submit">Register</button>
-        </form>
+        <Formik
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+        >
+            <Form>
+                <div>
+                    <label htmlFor="username">Username:</label>
+                    <Field 
+                        type="text" 
+                        id="username" 
+                        name="username"
+                    />
+                    <ErrorMessage name="username">
+                        {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                    </ErrorMessage>
+                </div>
+                <div>
+                    <label htmlFor="email">Email:</label>
+                    <Field 
+                        type="email" 
+                        id="email" 
+                        name="email"
+                    />
+                    <ErrorMessage name="email">
+                        {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                    </ErrorMessage>
+                </div>
+                <div>
+                    <label htmlFor="password">Password:</label>
+                    <Field 
+                        type="password" 
+                        id="password" 
+                        name="password"
+                    />
+                    <ErrorMessage name="password">
+                        {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                    </ErrorMessage>
+                </div>
+                <button type="submit">Register</button>
+            </Form>
+        </Formik>
     );
 }
 
