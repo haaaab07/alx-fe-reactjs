6bc194eabf9b3c81ee71bf11d85e4ad6f438a2a1
habib <bigharassane.elhabib@gmail.com>
2024-09-01 20:20:26 +0100

    ggg

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -11,12 +11,12 @@ function FormikForm() {
         },
         validationSchema: Yup.object({
             username: Yup.string()
-                .required('Required'),
+                .required('Username is required'),
             email: Yup.string()
                 .email('Invalid email address')
-                .required('Required'),
+                .required('Email is required'),
             password: Yup.string()
-                .required('Required')
+                .required('Password is required')
         }),
         onSubmit: values => {
             console.log('Form submitted', values);
@@ -26,28 +26,37 @@ function FormikForm() {
     return (
         <form onSubmit={formik.handleSubmit}>
             <div>
-                <label>Username:</label>
-                <input 
-                    type="text" 
+                <label htmlFor="username">Username:</label>
+                <input
+                    id="username"
+                    type="text"
                     {...formik.getFieldProps('username')}
                 />
-                <ErrorMessage name="username" component="div" />
+                <ErrorMessage name="username">
+                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
             </div>
             <div>
-                <label>Email:</label>
-                <input 
-                    type="email" 
+                <label htmlFor="email">Email:</label>
+                <input
+                    id="email"
+                    type="email"
                     {...formik.getFieldProps('email')}
                 />
-                <ErrorMessage name="email" component="div" />
+                <ErrorMessage name="email">
+                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
             </div>
             <div>
-                <label>Password:</label>
-                <input 
-                    type="password" 
+                <label htmlFor="password">Password:</label>
+                <input
+                    id="password"
+                    type="password"
                     {...formik.getFieldProps('password')}
                 />
-                <ErrorMessage name="password" component="div" />
+                <ErrorMessage name="password">
+                    {msg => <div style={{ color: 'red' }}>{msg}</div>}
+                </ErrorMessage>
             </div>
             <button type="submit">Register</button>
         </form>
