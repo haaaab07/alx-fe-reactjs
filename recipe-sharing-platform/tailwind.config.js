4b94784eadd888eec10ca89c67593e5ec8e2244f
habib <bigharassane.elhabib@gmail.com>
2024-09-15 16:31:50 +0100

    fddf

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Enable purging of unused styles
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   theme: {
     extend: {},
