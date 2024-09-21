29a32a5db0e85aef2dd9b0e534c30739f9347147
habib <bigharassane.elhabib@gmail.com>
2024-09-21 20:48:33 +0100

    efef

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Search from './components/Search';
 
 function App() {
   return (
     <div>
       <h1>GitHub User Search</h1>
-      <Search />
+      <p>Test Render</p>
     </div>
   );
 }
